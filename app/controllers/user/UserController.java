0162abbc3278e821d7248b66de55dca7a3f52d0f
Tobias Blaser <tobiasblaser@colvarim.ch>
2014-10-15 11:37:07 +0200

    REST API doc proposal

--- app/controllers/user/UserController.java
+++ app/controllers/user/UserController.java
@@ -8,6 +8,9 @@ import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+/**
+ * @Path("/user")
+ */
 public class UserController extends Controller {
 
 	public static final UserLogic USER_LOGIC = new UserLogic();
@@ -38,6 +41,14 @@ public class UserController extends Controller {
 		return ok(USER_LOGIC.getAsJson(user));
 	}
 
+	/**
+	 * @POST
+	 * @Path("/register")
+	 * @QueryParam("name") string username
+	 * @QueryParam("password") string password
+	 * @QueryParam("password_repeat") string password repetition
+	 * @return User
+	 */
 	@Transactional()
 	public static Result register() {
 		DynamicForm requestData = Form.form().bindFromRequest();
