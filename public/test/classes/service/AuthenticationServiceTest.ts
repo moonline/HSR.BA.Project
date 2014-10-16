adf1f0b54d9d72c2af4f0bb6c7829dbb9917b4ba
Tobias Blaser <tobiasblaser@colvarim.ch>
2014-10-16 08:53:22 +0200

    Complete AuthenticationService tests

--- public/test/classes/service/AuthenticationServiceTest.ts
+++ public/test/classes/service/AuthenticationServiceTest.ts
@@ -21,62 +21,139 @@ module test {
 
 			}));
 
-			it("No user logged in", function() {
-				httpBackend.when("GET", '/user/login-status').respond({});
-				var authentivationService: core.AuthenticationService = new core.AuthenticationService(http, rootScope, q);
+			describe("Status", function() {
+				it("Not logged in", function() {
+					httpBackend.when("GET", '/user/login-status').respond({});
+					var authentivationService: core.AuthenticationService = new core.AuthenticationService(http, rootScope, q);
 
-				expect(authentivationService.isLoggedIn).toEqual(false);
-				expect(authentivationService.currentUser).toEqual(null);
+					expect(authentivationService.isLoggedIn).toEqual(false);
+					expect(authentivationService.currentUser).toEqual(null);
 
-				var returnValue;
-				authentivationService.loginStatus(function(user) {
-					returnValue = user;
+					var returnValue;
+					authentivationService.loginStatus(function(user) {
+						returnValue = user;
+					});
+					httpBackend.flush();
+					expect(returnValue).toEqual(null);
+					expect(authentivationService.isLoggedIn).toEqual(false);
+					expect(authentivationService.currentUser).toEqual(null);
 				});
-				httpBackend.flush();
-				expect(returnValue).toEqual(null);
-				expect(authentivationService.isLoggedIn).toEqual(false);
-				expect(authentivationService.currentUser).toEqual(null);
 			});
 
-			it("User register successfull", function() {
-				var testUser: any = { "id": 5, "name": "peter" };
+			describe("Login / Logout", function() {
+				it("Successful login & successful logout", function() {
+					var testUserData: any = { "id": 5, "name": "peter" };
 
-				httpBackend.when("GET", '/user/login-status').respond({});
-				httpBackend.when("POST", '/user/register').respond(testUser);
+					httpBackend.when("GET", '/user/login-status').respond({});
+					httpBackend.when("POST", '/user/login').respond(testUserData);
+					httpBackend.when("POST", '/user/logout').respond(200);
 
-				var returnValue;
-				var authentivationService: core.AuthenticationService = new core.AuthenticationService(http, rootScope, q);
-				authentivationService.register(testUser.name, "pwd", "pwd", function(status, user) {
-					returnValue = { "status": status, "user": user };
+					var returnValue;
+					var authentivationService: core.AuthenticationService = new core.AuthenticationService(http, rootScope, q);
+					authentivationService.login(testUserData.name, "pwd", function(status, user) {
+						returnValue = { "status": status, "user": user };
+					});
+					httpBackend.flush();
+					expect(returnValue).toEqual({ status: true, user: core.User.createFromJson(testUserData) });
+					expect(authentivationService.isLoggedIn).toEqual(true);
+					expect(authentivationService.currentUser).toEqual(core.User.createFromJson(testUserData));
+
+					authentivationService.logout(function(status) {
+						returnValue = status;
+					});
+					httpBackend.flush();
+					expect(returnValue).toEqual(true);
+					expect(authentivationService.isLoggedIn).toEqual(false);
+					expect(authentivationService.currentUser).toEqual(null);
 				});
-				httpBackend.flush();
-				expect(returnValue).toEqual({ status: true, user: core.User.createFromJson(testUser) });
+
+				it("Successful login & failed logout", function() {
+					var testUserData: any = { "id": 5, "name": "peter" };
+
+					httpBackend.when("GET", '/user/login-status').respond({});
+					httpBackend.when("POST", '/user/login').respond(testUserData);
+					httpBackend.when("POST", '/user/logout').respond(403);
+
+					var returnValue;
+					var authentivationService: core.AuthenticationService = new core.AuthenticationService(http, rootScope, q);
+					authentivationService.login(testUserData.name, "pwd", function(status, user) {
+						returnValue = { "status": status, "user": user };
+					});
+					httpBackend.flush();
+					expect(returnValue).toEqual({ status: true, user: core.User.createFromJson(testUserData) });
+					expect(authentivationService.isLoggedIn).toEqual(true);
+					expect(authentivationService.currentUser).toEqual(core.User.createFromJson(testUserData));
+
+					authentivationService.logout(function(status) {
+						returnValue = status;
+					});
+					httpBackend.flush();
+					expect(returnValue).toEqual(false);
+					expect(authentivationService.isLoggedIn).toEqual(true);
+					expect(authentivationService.currentUser).toEqual(core.User.createFromJson(testUserData));
+				});
+
+				it("Failed login", function() {
+					var testUserData: any = { "id": 5, "name": "peter" };
+
+					httpBackend.when("GET", '/user/login-status').respond({});
+					httpBackend.when("POST", '/user/login').respond(403);
+
+					var returnValue;
+					var authentivationService: core.AuthenticationService = new core.AuthenticationService(http, rootScope, q);
+					authentivationService.login(testUserData.name, "pwd", function(status, user) {
+						returnValue = { "status": status, "user": user };
+					});
+					httpBackend.flush();
+					expect(returnValue).toEqual({ status: false, user: null });
+					expect(authentivationService.isLoggedIn).toEqual(false);
+					expect(authentivationService.currentUser).toEqual(null);
+				});
+
+
 			});
 
-			it("User register empty user returned", function() {
-				httpBackend.when("GET", '/user/login-status').respond({});
-				httpBackend.when("POST", '/user/register').respond({});
+			describe("Registration", function() {
+				it("Successful registration", function() {
+					var testUser: any = { "id": 5, "name": "peter" };
 
-				var returnValue;
-				var authentivationService: core.AuthenticationService = new core.AuthenticationService(http, rootScope, q);
-				authentivationService.register("peter", "pwd", "pwd", function(status, user) {
-					returnValue = { "status": status, "user": user };
+					httpBackend.when("GET", '/user/login-status').respond({});
+					httpBackend.when("POST", '/user/register').respond(testUser);
+
+					var returnValue;
+					var authentivationService: core.AuthenticationService = new core.AuthenticationService(http, rootScope, q);
+					authentivationService.register(testUser.name, "pwd", "pwd", function(status, user) {
+						returnValue = { "status": status, "user": user };
+					});
+					httpBackend.flush();
+					expect(returnValue).toEqual({ status: true, user: core.User.createFromJson(testUser) });
 				});
-				httpBackend.flush();
-				expect(returnValue).toEqual({ status: true, user: core.User.createFromJson({}) });
-			});
 
-			it("User register fail", function() {
-				httpBackend.when("GET", '/user/login-status').respond({});
-				httpBackend.when("POST", '/user/register').respond(403);
+				it("Malformed data returned", function() {
+					httpBackend.when("GET", '/user/login-status').respond({});
+					httpBackend.when("POST", '/user/register').respond({});
 
-				var returnValue;
-				var authentivationService: core.AuthenticationService = new core.AuthenticationService(http, rootScope, q);
-				authentivationService.register("peter", "pwd", "pwd", function(status, user) {
-					returnValue = { "status": status, "user": user };
+					var returnValue;
+					var authentivationService: core.AuthenticationService = new core.AuthenticationService(http, rootScope, q);
+					authentivationService.register("peter", "pwd", "pwd", function(status, user) {
+						returnValue = { "status": status, "user": user };
+					});
+					httpBackend.flush();
+					expect(returnValue).toEqual({ status: true, user: core.User.createFromJson({}) });
 				});
-				httpBackend.flush();
-				expect(returnValue).toEqual({ status: false, user: null });
+
+				it("Failed registration", function() {
+					httpBackend.when("GET", '/user/login-status').respond({});
+					httpBackend.when("POST", '/user/register').respond(403);
+
+					var returnValue;
+					var authentivationService: core.AuthenticationService = new core.AuthenticationService(http, rootScope, q);
+					authentivationService.register("peter", "pwd", "pwd", function(status, user) {
+						returnValue = { "status": status, "user": user };
+					});
+					httpBackend.flush();
+					expect(returnValue).toEqual({ status: false, user: null });
+				});
 			});
 		});
 	}
