e745fd1546eb61e83d743d12503b585bfc7d181c
Laurin Murer <hsr@laurinmurer.ch>
2014-10-22 18:33:59 +0200

    Fixed test that threw exception without failing

--- test/selenium/SeleniumExampleTest.java
+++ test/selenium/SeleniumExampleTest.java
@@ -1,7 +1,9 @@
 package selenium;
 
+import com.google.common.base.Predicate;
 import org.junit.Test;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.fest.assertions.Assertions.assertThat;
 
 public class SeleniumExampleTest extends AbstractSeleniumTest {
@@ -9,7 +11,11 @@ public class SeleniumExampleTest extends AbstractSeleniumTest {
 	@Test
 	public void exampleTest() {
 		browser.goTo("/");
+		//noinspection RedundantCast
+		browser.await().atMost(30, SECONDS).until((Predicate) (Object input) -> browser.pageSource().contains("password repeat"));
 		assertThat(browser.pageSource()).contains("EEPPI");
+		assertThat(browser.pageSource()).contains("Login");
+		assertThat(browser.pageSource()).contains("password repeat");
 	}
 
 }
