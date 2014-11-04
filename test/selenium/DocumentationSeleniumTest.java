98d605561a29e526503616fbde782bde55b2c6b7
Laurin Murer <hsr@laurinmurer.ch>
2014-11-05 00:11:04 +0100

    Fixed Documentation test

--- test/selenium/DocumentationSeleniumTest.java
+++ test/selenium/DocumentationSeleniumTest.java
@@ -11,7 +11,7 @@ public class DocumentationSeleniumTest extends AbstractSeleniumTest {
 	public void testDocumentation() throws Throwable {
 		browser.goTo(routes.DocumentationController.getAPIDocumentation().url());
 		assertThat(browser.pageSource()).contains("API Documentation for EEPPI");
-		assertThat(browser.pageSource()).contains("GET /dks/getFromDKS?url=&lt;id&gt;");
+		assertThat(browser.pageSource()).contains("GET /dks/getFromDKS?url=&lt;url&gt;");
 		assertThat(browser.pageSource()).contains("user</span><span class=\"pun\">/</span><span class=\"pln\">pptAccount</span><span class=\"pun\">/</span><span class=\"lit\">3");
 		assertThat(browser.pageSource()).doesNotContain(routes.DocumentationController.getAPIDocumentation().url());
 	}
