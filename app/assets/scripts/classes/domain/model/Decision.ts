7bfd9c9ea7f99ce188d9606e4120e4d3de1eec61
Tobias Blaser <tobiasblaser@colvarim.ch>
2014-11-14 19:41:50 +0100

    Update decision & option

--- app/assets/scripts/classes/domain/model/Decision.ts
+++ app/assets/scripts/classes/domain/model/Decision.ts
@@ -1,39 +1,36 @@
 /// <reference path='../../domain/model/Node.ts' />
+/// <reference path='../../domain/model/DksNode.ts' />
+/// <reference path='../../domain/model/Problem.ts' />
+
 /// <reference path='../../domain/repository/PersistentEntity.ts' />
 /// <reference path='../../domain/factory/FactoryConfiguration.ts' />
 
 /// <reference path='Option.ts' />
 
 module app.domain.model.dks {
-	export class Decision implements app.domain.model.dks.Node, app.domain.repository.core.PersistentEntity {
+	export class Decision extends app.domain.model.dks.DksNode implements app.domain.model.dks.Node, app.domain.repository.core.PersistentEntity {
 		public static factoryConfiguration: app.domain.factory.FactoryConfiguration = {
 			constructorArguments: [
 				{ name: "name", type: String, subType: null },
-				{ name: "alternatives", type: Array, subType: Option }
+				{ name: "path", type: Array, subType: String },
+				{ name: "attributes", type: Object, subType: null },
+				{ name: "notes", type: String, subType: null }
 			],
 			publicProperties: [
 				{ name: "id", type: Number, subType: null },
-				{ name: "state", type: String, subType: null },
 				{ name: "self", type: String, subType: null },
-				{ name: "notes", type: String, subType: null },
-				{ name: "attributes", type: Object, subType: null },
-				{ name: "path", type: Array, subType: String }
+				{ name: "alternatives", type: Array, subType: app.domain.model.dks.Option },
+				{ name: "template", type: app.domain.model.dks.Problem, subType: null },
+				{ name: "state", type: String, subType: null }
 			]
 		};
 
-		public id: number;
-		public name: string;
-		public self: string;
-		public notes: string;
-		public attibutes: Object;
-		public path: string[];
-		public options;
+		public alternatives: app.domain.model.dks.Option[];
+		public template: app.domain.model.dks.Problem;
 		public state: string;
 
-		constructor(name: string, options = []) {
-			this.id = Math.round(Math.random()*1000000);
-			this.name = name;
-			this.options = options;
+		constructor(name: string, path: string[], attributes: Object, notes: string) {
+			super(name, path, attributes, notes);
 		}
 	}
 }
