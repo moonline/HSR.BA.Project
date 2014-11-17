5b300c3ca24c7f04a9b3027582a03b13753d8aa6
Tobias Blaser <tobiasblaser@colvarim.ch>
2014-11-17 12:17:40 +0100

    Implement transmit

--- app/assets/scripts/classes/application/TransmissionController.ts
+++ app/assets/scripts/classes/application/TransmissionController.ts
@@ -162,59 +162,34 @@ var requestTemplate = '{\n\
 
 						var templateProcessor = new app.service.TemplateProcesser(exportDecisionData, $scope.currentRequestTemplate.requestBody, {});
 						var renderedTemplate = templateProcessor.process();
-						$scope.exportRequests.push({ requestBody: templateProcessor.process(), decision: decision, taskTemplate: mapping.taskTemplate });
+						$scope.exportRequests.push({
+							requestBody: templateProcessor.process(),
+							decision: decision,
+							taskTemplate: mapping.taskTemplate,
+							exportState: app.application.ApplicationState.waiting
+						});
 					});
 				});
 			};
 
 			$scope.transmit = function() {
+				var url = '/ppt/createPPTTask';
 
+				$scope.exportRequests.forEach(function(exportRequest, index){
+					$http.post(
+						url,
+						{ account: $scope.targetPPTAccount, path: $scope.currentRequestTemplate.url, content: exportRequest.requestBody, taskTemplate: exportRequest.taskTemplate, "taskProperties[]":null, project: null }
+					).success(function(data, status, headers, config){
+							exportRequest.exportState = app.application.ApplicationState.successful;
+						}.bind(this)).error(function(data, status, headers, config) {
+							exportRequest.exportState = app.application.ApplicationState.failed;
+						}.bind(this));
+				});
 			};
 
 			$scope.nextStep = function() {
 				$scope.currentWizzardStep++;
 			};
-
-			/*$scope.url = "http://localhost:9920/rest/api/2/issue/";
-			$scope.data = '{\n\t"fields": {\n\t\t"project": {\n\t\t\t"key": "TEST"\n\t\t},\n\t\t"assignee": "${assignee}",\n\t\t"description": "${description}",\n\t\t"issuetype": {\n\t\t\t"name": "${type}"\n\t\t}\n\t}\n}';
-			$scope.output = [];*/
-
-			/*$scope.render = function(text: string) {
-				$scope.output = [];
-				$scope.mappings.forEach(function(mapping){
-					var decisionTaskTemplates = [];
-
-					mapping.taskTemplates.forEach(function(taskTemplate){
-						var propertyValues = taskTemplate.getPropertieValuesByProperty();
-						console.log(propertyValues);
-						var textToReplace = ""+text;
-
-						var pattern = /\$\{\w*\}/igm;
-						for(var match; match = pattern.exec(textToReplace); ) {
-							var property: string = match[0].substring(2,match[0].length-1);
-							var replaceLength:number = match[0].length;
-							var index: number = match.index;
-							var replacer:string = (propertyValues[property] != undefined) ? propertyValues[property].value : "";
-							textToReplace = textToReplace.slice(0, index) + replacer + textToReplace.slice(index+replaceLength, textToReplace.length);
-						}
-						decisionTaskTemplates.push(textToReplace);
-					});
-					$scope.output.push(decisionTaskTemplates);
-				});
-			};*/
-
-			/*$scope.transmit = function() {
-				$scope.trasmitCount = 0;
-				$scope.transmitSuccessfullCount = 0;
-				$scope.output.forEach(function(decisionTemplates) {
-					decisionTemplates.forEach(function(decisionTaskTemplate){
-						$scope.trasmitCount++;
-						$http.post($scope.url, decisionTaskTemplate).success(function() {
-							$scope.transmitSuccessfullCount++;
-						});
-					});
-				});
-			};*/
 		}
 	}
 }
