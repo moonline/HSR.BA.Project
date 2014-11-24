41a0a2bd02d0b120588e2c6a96ef2e66816f803b
Tobias Blaser <tobiasblaser@colvarim.ch>
2014-11-24 14:21:42 +0100

    Refactoring: Extract long methods in TransmissionController to parts

--- app/assets/scripts/classes/application/TransmissionController.ts
+++ app/assets/scripts/classes/application/TransmissionController.ts
@@ -28,6 +28,8 @@ module app.application {
 
 		constructor($scope, $location, persistenceService, authenticationService, $http) {
 			this.$scope = $scope;
+			var self = this;
+
 			$scope.ExportWizardSteps = ExportWizardSteps;
 			$scope.currentWizardStep = ExportWizardSteps.ToolSelection;
 
@@ -80,23 +82,7 @@ module app.application {
 						$scope.currentProject = projects[0] || null;
 
 						processorRepository.findAll(function(success, processorList){
-							processorList.forEach(function(processor) {
-								"use strict";
-
-								// evaluate processor from text -- eval is a critical function because of security
-								// but it gives the user the most powerful processor functionality
-								try {
-									var processorCode: string = null;
-
-									var code = '(function () { return '+processor.code+' })()';
-									processorCode = eval.call(null, code);
-
-									processors[processor.name] = processorCode;
-								} catch (e) {
-									console.error(processor.name+"() processor is not valid code!");
-									// who cares. It's the problem of the user, if he writes incorrect processors
-								}
-							});
+							self.evaluateProcessors(processorList, processors);
 
 							decisionKnowledgeSystemRepository.findAll(function(success, items) {
 								$scope.currentDks = <app.domain.model.dks.DecisionKnowledgeSystem>items[0];
@@ -107,63 +93,13 @@ module app.application {
 									$scope.decisions = items;
 
 									// fill decisions sorted by problem into decisionMappings
-									for(var di in $scope.decisions) {
-										var decision = $scope.decisions[di];
-										if(decision && decision.template) {
-											if(!$scope.decisionMappings[decision.template.id]) {
-												$scope.decisionMappings[decision.template.id] = { problem: decision.template, decisions: {} };
-											}
-											$scope.decisionMappings[decision.template.id]['decisions'][decision.id] = {
-												decision: decision,
-												taskTemplatesToExport: {},
-												rootElementForSubNodes: null,
-												mappings: {},
-												alternatives: {}
-											};
-											// fill alternatives into decisionMappings[decision][alternatives]
-											for(var ai in decision.alternatives) {
-												var alternative = decision.alternatives[ai];
-												if(alternative && alternative.template) {
-													$scope.decisionMappings[decision.template.id]['decisions'][decision.id]['alternatives'][alternative.id] = {
-														alternative: alternative,
-														alternativeTemplate: alternative.template,
-														mappings: {}
-													};
-												}
-											}
-										}
-									}
+									self.orderDecisionsByProblem($scope);
 
 									mappingRepository.findAll(function(success, items) {
 										$scope.mappings = items;
 
 										setTimeout(() => { $scope.operationState = app.application.ApplicationState.successful; $scope.$apply(); }, configuration.settings.messageBoxDelay);
-
-										for(var mi in $scope.mappings) {
-											var mapping = $scope.mappings[mi];
-											// only add mapping, if decision exist
-											if($scope.decisionMappings[mapping.dksNode]) {
-												for(var dmi in $scope.decisionMappings[mapping.dksNode]['decisions']) {
-													var decisionElement = $scope.decisionMappings[mapping.dksNode]['decisions'][dmi];
-													decisionElement.mappings[mapping.id] = mapping;
-												}
-											// find alternatives with mappings
-											} else {
-												for(var pi in $scope.decisionMappings) {
-													for(var di in $scope.decisionMappings[pi]['decisions']) {
-														for(var ai in $scope.decisionMappings[pi]['decisions'][di]['alternatives']) {
-															var alternativeElement = $scope.decisionMappings[pi]['decisions'][di]['alternatives'][ai];
-															// if the decision is solved - take only the chosen alternative, otherwise take all
-															if(alternativeElement.alternative.template.id == mapping.dksNode &&
-																($scope.decisionMappings[pi]['decisions'][di].decision.state != "Solved" ||
-																	alternativeElement.alternative.state == "Chosen")) {
-																alternativeElement.mappings[mapping.id] = mapping;
-															}
-														}
-													}
-												}
-											}
-										}
+										self.findDecisionsWithMappings($scope);
 									});
 								});
 							});
@@ -216,60 +152,11 @@ module app.application {
 
 					// decisions
 					Object.keys(decisionGroup.decisions).forEach(function(deKey) {
-						var decision:app.domain.model.dks.Decision = decisionGroup.decisions[deKey].decision;
 						var taskTemplatesToExport = decisionGroup.decisions[deKey].taskTemplatesToExport;
-
-						Object.keys(decisionGroup.decisions[deKey].mappings).forEach(function(mKey) {
-							if(taskTemplatesToExport[mKey] && taskTemplatesToExport[mKey] === true) {
-								var mapping:app.domain.model.core.Mapping = decisionGroup.decisions[deKey].mappings[mKey];
-
-								// transform propertyValues list to dictionary to allow simple access using processor variables
-								if(!mapping.taskTemplate['attributes']) {
-									mapping.taskTemplate['attributes'] = {};
-									mapping.taskTemplate.properties.forEach(function(taskPropertyValue, index){
-										mapping.taskTemplate['attributes'][taskPropertyValue.property.name.toLowerCase()] = taskPropertyValue.value;
-									});
-								}
-
-								if(!$scope.transmitNodes[deKey]) {
-									$scope.transmitNodes[deKey] = { node: decision, type: app.domain.model.dks.Decision, mappings: [], subNodes: {} };
-								}
-								$scope.transmitNodes[deKey].mappings.push(mapping);
-								if(mapping == decisionGroup.decisions[deKey].rootElementForSubNodes) {
-									$scope.transmitNodes[deKey].rootElementForSubNodes = mapping;
-								}
-							}
-						});
-
+						self.addTaskTemplatesOfSelectedDecisionsToTransmissionNodes(decisionGroup, deKey, taskTemplatesToExport, $scope);
 						// alternatives
 						Object.keys(decisionGroup.decisions[deKey].alternatives).forEach(function(aeKey) {
-							var alternative: app.domain.model.dks.Alternative = decisionGroup.decisions[deKey].alternatives[aeKey].alternative;
-
-							Object.keys(decisionGroup.decisions[deKey].alternatives[aeKey].mappings).forEach(function(amKey) {
-								if(taskTemplatesToExport[amKey] && taskTemplatesToExport[amKey] === true) {
-									var mapping:app.domain.model.core.Mapping = decisionGroup.decisions[deKey].alternatives[aeKey].mappings[amKey];
-
-									// transform propertyValues list to dictionary to allow simple access using processor variables
-									if(!mapping.taskTemplate['attributes']) {
-										mapping.taskTemplate['attributes'] = {};
-										mapping.taskTemplate.properties.forEach(function(taskPropertyValue, index){
-											mapping.taskTemplate['attributes'][taskPropertyValue.property.name.toLowerCase()] = taskPropertyValue.value;
-										});
-									}
-
-									if($scope.transmitNodes[deKey]) {
-										if(!$scope.transmitNodes[deKey].subNodes[aeKey]) {
-											$scope.transmitNodes[deKey].subNodes[aeKey] = { node: alternative, type: app.domain.model.dks.Option, mappings: []}
-										}
-										$scope.transmitNodes[deKey].subNodes[aeKey].mappings.push(mapping);
-									} else {
-										if(!$scope.transmitNodes[aeKey]) {
-											$scope.transmitNodes[aeKey] = { node: alternative, type: app.domain.model.dks.Option, mappings: [], subNodes: {} };
-										}
-										$scope.transmitNodes[aeKey].mappings.push(mapping);
-									}
-								}
-							});
+							self.addTaskTemplatesOfSelectedAlternativesToTransmissionNodes(decisionGroup, deKey, aeKey, taskTemplatesToExport, $scope);
 						});
 					});
 				});
@@ -280,74 +167,11 @@ module app.application {
 					var exportRequest = <any>null;
 
 					Object.keys($scope.transmitNodes[dKey].mappings).forEach(function(tKey){
-						var mapping = $scope.transmitNodes[dKey].mappings[tKey];
-
-						var exportDecisionData = {
-							node: node,
-							taskTemplate: mapping.taskTemplate,
-							currentUser: authenticationService.loggedInUser,
-							project: $scope.currentProject,
-							pptProject: $scope.pptProject,
-							mappings: $scope.decisionMappings
-						};
-
-						var templateProcessor = new app.service.TemplateProcesser(exportDecisionData, $scope.currentRequestTemplate.requestBodyTemplate, processors);
-						var renderedTemplate;
-						try {
-							renderedTemplate = templateProcessor.process();
-							var renderedTemplate = templateProcessor.process();
-							var currentRequest = {
-								requestBody: renderedTemplate,
-								node: node,
-								taskTemplate: mapping.taskTemplate,
-								exportState: app.application.ApplicationState.waiting
-							};
-							if($scope.transmitNodes[dKey].rootElementForSubNodes == mapping) {
-								exportRequest = currentRequest;
-							}
-							$scope.exportRequests.push(currentRequest);
-						} catch (error) {
-							$scope.transformationErrors.push("Errors occured during executing processors. Please check the code of your processors!");
-						}
+						exportRequest = self.renderRequestTemplates($scope, dKey, tKey, node, authenticationService, processors, exportRequest);
 					});
 
 					Object.keys($scope.transmitNodes[dKey].subNodes).forEach(function(aKey){
-						var subNode = $scope.transmitNodes[dKey].subNodes[aKey].node;
-
-						Object.keys($scope.transmitNodes[dKey].subNodes[aKey].mappings).forEach(function(atKey){
-							var mapping = $scope.transmitNodes[dKey].subNodes[aKey].mappings[atKey];
-
-							var exportDecisionData = {
-								node: subNode,
-								taskTemplate: mapping.taskTemplate,
-								currentUser: authenticationService.loggedInUser,
-								project: $scope.currentProject,
-								pptProject: $scope.pptProject,
-								mappings: $scope.decisionMappings
-							};
-
-							var templateProcessor = new app.service.TemplateProcesser(exportDecisionData, $scope.currentRequestTemplate.requestBodyTemplate, processors);
-							var renderedTemplate;
-							try {
-								renderedTemplate = templateProcessor.process();
-
-
-								var currentSubRequest = {
-									requestBody: renderedTemplate,
-									node: subNode,
-									taskTemplate: mapping.taskTemplate,
-									exportState: app.application.ApplicationState.waiting
-								};
-								if(exportRequest) {
-									if(!exportRequest.subRequests) { exportRequest.subRequests = []; }
-									exportRequest.subRequests.push(currentSubRequest);
-								} else {
-									$scope.exportRequests.push(currentSubRequest);
-								}
-							} catch (error) {
-								$scope.transformationErrors.push("Errors occured during executing processors. Please check the code of your processors!");
-							}
-						});
+						self.renderSubRequestTemplate($scope, dKey, aKey, authenticationService, processors, exportRequest);
 					});
 				});
 			};
@@ -404,6 +228,213 @@ module app.application {
 			$scope.nextStep = function() {
 				$scope.currentWizardStep++;
 			};
+		}
+
+		evaluateProcessors(processorList, processors) {
+			processorList.forEach(function (processor) {
+				"use strict";
+
+				// evaluate processor from text -- eval is a critical function because of security
+				// but it gives the user the most powerful processor functionality
+				try {
+					var processorCode:string = null;
+
+					var code = '(function () { return ' + processor.code + ' })()';
+					processorCode = eval.call(null, code);
+
+					processors[processor.name] = processorCode;
+				} catch (e) {
+					console.error(processor.name + "() processor is not valid code!");
+					// who cares. It's the problem of the user, if he writes incorrect processors
+				}
+			});
+		}
+
+		orderDecisionsByProblem($scope) {
+			for (var di in $scope.decisions) {
+				var decision = $scope.decisions[di];
+				if (decision && decision.template) {
+					if (!$scope.decisionMappings[decision.template.id]) {
+						$scope.decisionMappings[decision.template.id] = { problem: decision.template, decisions: {} };
+					}
+					$scope.decisionMappings[decision.template.id]['decisions'][decision.id] = {
+						decision: decision,
+						taskTemplatesToExport: {},
+						rootElementForSubNodes: null,
+						mappings: {},
+						alternatives: {}
+					};
+					// fill alternatives into decisionMappings[decision][alternatives]
+					for (var ai in decision.alternatives) {
+						var alternative = decision.alternatives[ai];
+						if (alternative && alternative.template) {
+							$scope.decisionMappings[decision.template.id]['decisions'][decision.id]['alternatives'][alternative.id] = {
+								alternative: alternative,
+								alternativeTemplate: alternative.template,
+								mappings: {}
+							};
+						}
+					}
+				}
+			}
+		}
+
+		findDecisionsWithMappings($scope) {
+			for (var mi in $scope.mappings) {
+				var mapping = $scope.mappings[mi];
+				// only add mapping, if decision exist
+				if ($scope.decisionMappings[mapping.dksNode]) {
+					for (var dmi in $scope.decisionMappings[mapping.dksNode]['decisions']) {
+						var decisionElement = $scope.decisionMappings[mapping.dksNode]['decisions'][dmi];
+						decisionElement.mappings[mapping.id] = mapping;
+					}
+					// find alternatives with mappings
+				} else {
+					for (var pi in $scope.decisionMappings) {
+						for (var di in $scope.decisionMappings[pi]['decisions']) {
+							for (var ai in $scope.decisionMappings[pi]['decisions'][di]['alternatives']) {
+								var alternativeElement = $scope.decisionMappings[pi]['decisions'][di]['alternatives'][ai];
+								// if the decision is solved - take only the chosen alternative, otherwise take all
+								if (alternativeElement.alternative.template.id == mapping.dksNode &&
+									($scope.decisionMappings[pi]['decisions'][di].decision.state != "Solved" ||
+										alternativeElement.alternative.state == "Chosen")) {
+									alternativeElement.mappings[mapping.id] = mapping;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		renderSubRequestTemplate($scope, dKey, aKey, authenticationService, processors, exportRequest) {
+			var subNode = $scope.transmitNodes[dKey].subNodes[aKey].node;
+
+			Object.keys($scope.transmitNodes[dKey].subNodes[aKey].mappings).forEach(function (atKey) {
+				var mapping = $scope.transmitNodes[dKey].subNodes[aKey].mappings[atKey];
+
+				var exportDecisionData = {
+					node: subNode,
+					taskTemplate: mapping.taskTemplate,
+					currentUser: authenticationService.loggedInUser,
+					project: $scope.currentProject,
+					pptProject: $scope.pptProject,
+					mappings: $scope.decisionMappings
+				};
+
+				var templateProcessor = new app.service.TemplateProcesser(exportDecisionData, $scope.currentRequestTemplate.requestBodyTemplate, processors);
+				var renderedTemplate;
+				try {
+					renderedTemplate = templateProcessor.process();
+
+
+					var currentSubRequest = {
+						requestBody: renderedTemplate,
+						node: subNode,
+						taskTemplate: mapping.taskTemplate,
+						exportState: app.application.ApplicationState.waiting
+					};
+					if (exportRequest) {
+						if (!exportRequest.subRequests) {
+							exportRequest.subRequests = [];
+						}
+						exportRequest.subRequests.push(currentSubRequest);
+					} else {
+						$scope.exportRequests.push(currentSubRequest);
+					}
+				} catch (error) {
+					$scope.transformationErrors.push("Errors occured during executing processors. Please check the code of your processors!");
+				}
+			});
+		}
+
+		renderRequestTemplates($scope, dKey, tKey, node, authenticationService, processors, exportRequest) {
+			var mapping = $scope.transmitNodes[dKey].mappings[tKey];
+
+			var exportDecisionData = {
+				node: node,
+				taskTemplate: mapping.taskTemplate,
+				currentUser: authenticationService.loggedInUser,
+				project: $scope.currentProject,
+				pptProject: $scope.pptProject,
+				mappings: $scope.decisionMappings
+			};
+
+			var templateProcessor = new app.service.TemplateProcesser(exportDecisionData, $scope.currentRequestTemplate.requestBodyTemplate, processors);
+			var renderedTemplate;
+			try {
+				renderedTemplate = templateProcessor.process();
+				var renderedTemplate = templateProcessor.process();
+				var currentRequest = {
+					requestBody: renderedTemplate,
+					node: node,
+					taskTemplate: mapping.taskTemplate,
+					exportState: app.application.ApplicationState.waiting
+				};
+				if ($scope.transmitNodes[dKey].rootElementForSubNodes == mapping) {
+					exportRequest = currentRequest;
+				}
+				$scope.exportRequests.push(currentRequest);
+			} catch (error) {
+				$scope.transformationErrors.push("Errors occured during executing processors. Please check the code of your processors!");
+			}
+			return exportRequest;
+		}
+
+		addTaskTemplatesOfSelectedAlternativesToTransmissionNodes(decisionGroup, deKey, aeKey, taskTemplatesToExport, $scope) {
+			var alternative:app.domain.model.dks.Alternative = decisionGroup.decisions[deKey].alternatives[aeKey].alternative;
+
+			Object.keys(decisionGroup.decisions[deKey].alternatives[aeKey].mappings).forEach(function (amKey) {
+				if (taskTemplatesToExport[amKey] && taskTemplatesToExport[amKey] === true) {
+					var mapping:app.domain.model.core.Mapping = decisionGroup.decisions[deKey].alternatives[aeKey].mappings[amKey];
+
+					// transform propertyValues list to dictionary to allow simple access using processor variables
+					if (!mapping.taskTemplate['attributes']) {
+						mapping.taskTemplate['attributes'] = {};
+						mapping.taskTemplate.properties.forEach(function (taskPropertyValue, index) {
+							mapping.taskTemplate['attributes'][taskPropertyValue.property.name.toLowerCase()] = taskPropertyValue.value;
+						});
+					}
+
+					if ($scope.transmitNodes[deKey]) {
+						if (!$scope.transmitNodes[deKey].subNodes[aeKey]) {
+							$scope.transmitNodes[deKey].subNodes[aeKey] = { node: alternative, type: app.domain.model.dks.Option, mappings: []}
+						}
+						$scope.transmitNodes[deKey].subNodes[aeKey].mappings.push(mapping);
+					} else {
+						if (!$scope.transmitNodes[aeKey]) {
+							$scope.transmitNodes[aeKey] = { node: alternative, type: app.domain.model.dks.Option, mappings: [], subNodes: {} };
+						}
+						$scope.transmitNodes[aeKey].mappings.push(mapping);
+					}
+				}
+			});
+		}
+
+		addTaskTemplatesOfSelectedDecisionsToTransmissionNodes(decisionGroup, deKey, taskTemplatesToExport, $scope) {
+			var decision:app.domain.model.dks.Decision = decisionGroup.decisions[deKey].decision;
+
+			Object.keys(decisionGroup.decisions[deKey].mappings).forEach(function (mKey) {
+				if (taskTemplatesToExport[mKey] && taskTemplatesToExport[mKey] === true) {
+					var mapping:app.domain.model.core.Mapping = decisionGroup.decisions[deKey].mappings[mKey];
+
+					// transform propertyValues list to dictionary to allow simple access using processor variables
+					if (!mapping.taskTemplate['attributes']) {
+						mapping.taskTemplate['attributes'] = {};
+						mapping.taskTemplate.properties.forEach(function (taskPropertyValue, index) {
+							mapping.taskTemplate['attributes'][taskPropertyValue.property.name.toLowerCase()] = taskPropertyValue.value;
+						});
+					}
+
+					if (!$scope.transmitNodes[deKey]) {
+						$scope.transmitNodes[deKey] = { node: decision, type: app.domain.model.dks.Decision, mappings: [], subNodes: {} };
+					}
+					$scope.transmitNodes[deKey].mappings.push(mapping);
+					if (mapping == decisionGroup.decisions[deKey].rootElementForSubNodes) {
+						$scope.transmitNodes[deKey].rootElementForSubNodes = mapping;
+					}
+				}
+			});
 		}
 	}
 }
